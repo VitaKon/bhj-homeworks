3a906d1ebdb0915c8bfcce6b68dd7b10104c6eaa
Vitaliy Kondrashov <v.kondrashov@list.ru>
2023-05-08 16:09:18 +0300

    Задание 1 выполнение

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+let timer = document.getElementById("timer");
+
+function timerMinus() {
+    if (timer.textContent <= 0) {
+        clearInterval(idInterval);
+        alert("Вы победили в конкурсе!");
+    } else {
+        timer.textContent--;
+    }
+};
+
+let idInterval = setInterval(timerMinus, 1000);
