7cf403cc13268e2b60a1cbb85afabe9b61f15875
Vitaliy Kondrashov <v.kondrashov@list.ru>
2023-05-10 18:00:17 +0300

    Задача 1 коррект

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,16 +1,35 @@
+const modalMain = document.getElementById('modal_main');
+const modalSuccess = document.getElementById('modal_success');
+const showSuccess = document.querySelector(".show-success")
+const modalClose = Array.from(document.querySelectorAll(".modal__close"))
+const btnSuccess = document.querySelector(".btn_success")
 
-let modalMain = document.getElementById('modal_main');
-let modalClose = Array.from(document.getElementsByClassName('modal__close'));
-let showSuccess = document.querySelector('.show-success');
-let modalSuccess = document.getElementById('modal_success');
 
 modalMain.classList.add('modal_active');
 
-modalClose.forEach(item => item.onclick = function () {
-    modalMain.classList.remove('modal_active');
-    modalSuccess.classList.remove('modal_active');
-});
-
 showSuccess.onclick = function () {
+    modalMain.classList.remove('modal_active')
     modalSuccess.classList.add('modal_active');
-};
+}
+
+for (let i = 0; i < modalClose.length; i++) {
+    modalClose[i].onclick = function () {
+        if (modalMain.className.includes('modal_active')) {
+            modalMain.classList.remove('modal_active')
+            modalSuccess.classList.add('modal_active')
+        } else {
+            modalSuccess.classList.remove('modal_active')
+        }
+
+    }
+}
+
+modalClose.onclick = function (element) {
+    modalMain.classList.remove('modal_active')
+    modalSuccess.classList.remove('modal_active');
+}
+
+btnSuccess.onclick = function () {
+    modalSuccess.classList.remove('modal_active')
+    modalMain.classList.remove('modal_active')
+}
